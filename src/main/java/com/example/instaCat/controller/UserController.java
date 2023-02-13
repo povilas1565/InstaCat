67be2bd4378ebd98268d27a5a3369059774c04f8
povilas1565 <pavel.ryzhkov.99@mail.ru>
2023-02-13 11:26:44 +0300

    Добавлен контроллеры: Post, Image, Comment.

---
+++ src/main/java/com/example/instaCat/controller/UserController.java
@@ -0,0 +1,68 @@
+package com.example.instaCat.controller;
+
+import com.example.instaCat.dto.UserDTO;
+import com.example.instaCat.entity.User;
+import com.example.instaCat.facade.UserFacade;
+import com.example.instaCat.repository.UserRepository;
+import com.example.instaCat.service.UserService;
+import com.example.instaCat.validators.ResponseErrorValidator;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.ObjectUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.security.Principal;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/user")
+@CrossOrigin //про аннотацию: https://russianblogs.com/article/9354767831/
+public class UserController {
+
+    @Autowired
+    private UserFacade userFacade;
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private ResponseErrorValidator responseErrorValidator;
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/")
+    public ResponseEntity<UserDTO> getCurrentUser(Principal principal) {
+        User user = userService.getCurrentUser(principal);
+        UserDTO userDTO = userFacade.userToUserDTO(user);
+
+        return new ResponseEntity<>(userDTO, HttpStatus.OK);
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserDTO> getUserProfile(@PathVariable("userId") String userId) {
+        User user = userService.getUserById(Long.parseLong(userId));
+        UserDTO userDTO = userFacade.userToUserDTO(user);
+
+        return new ResponseEntity<>(userDTO, HttpStatus.OK);
+    }
+
+    @PostMapping("/update")
+    @Operation(summary = "updateUser", security = @SecurityRequirement(name = "bearerAuth"))
+    public ResponseEntity<Object> updateUser(@Valid @RequestBody UserDTO userDTO, BindingResult bindingResult, Principal principal) {
+        ResponseEntity<Object> listErrors = responseErrorValidator.mappedValidatorService(bindingResult);
+        if (!ObjectUtils.isEmpty(listErrors)) return listErrors;
+
+        User user = userService.updateUser(userDTO, principal);
+        UserDTO userUpdated = userFacade.userToUserDTO(user);
+
+        return new ResponseEntity<>(userUpdated, HttpStatus.OK);
+    }
+
+    @GetMapping("/allUser")
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+}
